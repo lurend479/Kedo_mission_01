8d6b5a06fb4f1aa1372a824bb4ef480091a251b9
DESKTOP-T2GM0H5\woong <lurend479>
2017-04-26 01:25:57 +0900

    코드 추가

---
+++ index.js
@@ -0,0 +1,96 @@
+var express = require('express');
+var request = require("request");  
+var cheerio = require("cheerio");
+var async = require("async");
+
+var newsFindkey = [
+  {
+    url : 'http://www.hani.co.kr/',
+    findkey : '.article-title',
+    plusurl : 'http://www.hani.co.kr/',
+    title : '한겨레'
+  },
+  {
+    url : 'http://newstapa.org/',
+    findkey : '.item',
+    plusurl : '',
+    title : '뉴스타파'
+  }
+];
+
+var app = express();
+app.use(express.static('public'));
+app.locals.pretty = true;  
+
+var titleText = [];
+var titleHerf = [];
+var loadComplete = 0;
+
+app.get('/', function(req, res){
+
+  loadComplete = 0;
+  var tasks = [];
+  var functionTEST = function(callback){
+        titleText.push(newsFindkey[loadComplete].title);
+        titleHerf.push(newsFindkey[loadComplete].url);
+        titleText.push('');
+        titleHerf.push('');
+        request(newsFindkey[loadComplete].url, function(error, response, body){
+          var returnValue = GetTitleHref(cheerio.load(body), newsFindkey[loadComplete].findkey, newsFindkey[loadComplete].plusurl);
+          
+          loadComplete++;
+          callback(null);
+        })
+  };
+        
+  for(var index = 0 ; index < newsFindkey.length ; ++index)
+  {
+    tasks.push(functionTEST);
+  }
+
+  async.waterfall(tasks, function (err) {
+      if (err)
+          console.log('err');
+      else
+          console.log('done');
+
+      var lis = '';
+      for(var i=0; i< titleText.length; i++){
+        lis = lis + '<li>'+ '<a href=' + titleHerf[i] + '>' + titleText[i] + '</li>';
+      }
+
+      var output = `
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <meta charset="utf-8">
+            <title></title>
+          </head>
+          <body>
+              Hello, Dynamic!
+              <ul>
+                ${lis}
+              </ul>
+          </body>
+        </html>`;
+        res.send(output);
+  });
+});
+
+function GetTitleHref(loadData, findKey, plusURL)
+{
+    var postElements = loadData(findKey);
+      postElements.each(function() {
+        var postTitle = loadData(this).find("a").text();
+        var postHref = loadData(this).find("a").attr("href");
+          
+        titleText.push(postTitle);
+        titleHerf.push(plusURL + postHref);
+    });
+
+    return true;
+}
+
+app.listen(process.env.PORT || 8080, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
